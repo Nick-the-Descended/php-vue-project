65b6bc7b4a882e0d514654d13a94023c56be0cf4
Nick-the-Descended <76739481+Nick-the-Descended@users.noreply.github.com>
2023-05-08 19:01:52 +0400

    adding files and making pre-final changes 2.1

--- my-vue-app/src/config.js
+++ my-vue-app/src/config.js
@@ -2,6 +2,6 @@ import {env} from "eslint-plugin-vue/lib/configs/base";
 
 const API_BASE_URL = env.VUE_APP_API_BASE_URL || 'https://php-vue-project.000webhostapp.com';
 
-export {
+export default {
     API_BASE_URL,
 };
